47d481affdcc1376b42461769ad04f48af19c201
Tran Duc Binh <binhtran432k@gmail.com>
2022-06-15 09:47:16 +0700

    First Commit

---
+++ GameOver/src/main.cpp
@@ -0,0 +1,9 @@
+// Game Over
+// A first C++ program
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Game Over!" << endl;
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Source for Beginning C++ Through Game Progamming
+
+This repository includes the source while learning the Beginning C++ Through
+Game Progamming.
